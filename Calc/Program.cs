bf4c86a001fbfff060d277dce11f6b49f02d1cf8
Soimo <jnr@bk.ru>
2018-10-18 17:31:50 +0300

    Commit

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -12,23 +12,47 @@ namespace Calc
         {
             Console.WriteLine("Калькулятор \n \n");
             string n = "1";
-            int m = 1;
+            int m ;
             do
 
             {
                 
                 Console.WriteLine("Введите первый операнд");
                 string x = Console.ReadLine();
-                double a = double.Parse(x);
+                //double a = double.Parse(x);
+                double a;
+                if (double.TryParse(x, out a))
+                {
+                    Console.WriteLine("\n Первый операнд: {0} \n", a);
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод. Операнд будет приведен к нулю \n");
+                    
+                }
+                
                 Console.WriteLine("Введите второй операнд");
                 string y = Console.ReadLine();
-                double b = double.Parse(y);
+                //double b = double.Parse(y);
+                double b;
+                if (double.TryParse(y, out b))
+                {
+                    Console.WriteLine("\n Второй операнд: {0} \n", b);
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод. Операнд будет приведен к нулю \n");
+                    
+                }
+
+
 
                 Console.WriteLine("Выберите операцию:");
                 Console.WriteLine("1 - Сложение");
                 Console.WriteLine("2 - Вычитание");
                 Console.WriteLine("3 - Умножение");
-                Console.WriteLine("4 - Деление");
+                Console.WriteLine("4 - Деление \n");
+                Console.WriteLine("0 - Выход");
                 string z = Console.ReadLine();
                 int c = int.Parse(z);
                 double d = 0;
@@ -52,9 +76,14 @@ namespace Calc
                         d = a / b;
                         break;
 
+                    case 0:
+                        System.Environment.Exit(0);
+                        break;
+
                     default:
 
                         Console.WriteLine("Введена некорректная команда");
+
                         break;
                 }
 
@@ -65,7 +94,7 @@ namespace Calc
                 
 
 
-            } while (m <= 1);
+            } while (m == 1);
 
             
 
