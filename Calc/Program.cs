62ca61ee11668ec63566dcd24b44fadf40ed4f59
Soimo <jnr@bk.ru>
2018-10-19 17:54:11 +0300

    Master

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -19,40 +19,14 @@ namespace Calc
                 
                 Console.WriteLine("Введите первый операнд");
                 string x = Console.ReadLine();
-                //double a = double.Parse(x);
-                double a;
-                if (double.TryParse(x, out a))
-                {
-                    Console.WriteLine("\n Первый операнд: {0} \n", a);
-                }
-                else
-                {
-                    Console.WriteLine("Неверный ввод. Операнд будет приведен к нулю \n");
-                    
-                }
-                
+                double a = double.Parse(x);
+                                
                 Console.WriteLine("Введите второй операнд");
                 string y = Console.ReadLine();
-                //double b = double.Parse(y);
-                double b;
-                if (double.TryParse(y, out b))
-                {
-                    Console.WriteLine("\n Второй операнд: {0} \n", b);
-                }
-                else
-                {
-                    Console.WriteLine("Неверный ввод. Операнд будет приведен к нулю \n");
-                    
-                }
-
-
-
+                double b = double.Parse(y);
+                                
                 Console.WriteLine("Выберите операцию:");
-                Console.WriteLine("1 - Сложение");
-                Console.WriteLine("2 - Вычитание");
-                Console.WriteLine("3 - Умножение");
-                Console.WriteLine("4 - Деление \n");
-                Console.WriteLine("0 - Выход");
+                Console.WriteLine("1 - Сложение \n 2 - Вычитание \n 3 - Умножение \n 4 - Деление \n \n 0 - Выход");
                 string z = Console.ReadLine();
                 int c = int.Parse(z);
                 double d = 0;
